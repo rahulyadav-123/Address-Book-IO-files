3f4e3d88acf401478e7ed6429442fef73dfce401
rahul yadav <ry219613@gmail.com>
2021-11-12 16:56:49 +0530

    Refactored code and added search the person by state and city

---
+++ com/bridgelab/addressbook/Sort.java
@@ -0,0 +1,13 @@
+package com.bridgelab.addressbook;
+
+import java.util.Comparator;
+
+public class Sort {
+	static Comparator<ContactPerson> compareCity = new Comparator<ContactPerson>() {
+        public int compare(ContactPerson one, ContactPerson two) {
+            return one.getCity().compareTo(two.getCity());
+        }
+    };
+}
+
+
